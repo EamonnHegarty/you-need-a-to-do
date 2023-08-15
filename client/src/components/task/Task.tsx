91bb0ce5638a620b55cf3c5e55a6a11002072266
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-08-15 15:31:24 +0100

    finished with front end prior to server

--- client/src/components/task/Task.tsx
+++ client/src/components/task/Task.tsx
@@ -3,8 +3,23 @@ import { Box } from '@mui/material';
 import { TaskHeader } from './TaskHeader';
 import { TaskDescription } from './TaskDescription';
 import { TaskFooter } from './TaskFooter';
+import { ITask } from './interfaces/ITask';
+import { Status } from '../createTaskForm/enums/Status';
+import { Priority } from '../createTaskForm/enums/Priority';
+import PropTypes from 'prop-types';
+import { renderPriorityBorderColor } from './helpers/renderPriorityBorderColor';
 
-export const Task: FC = (props): ReactElement => {
+export const Task: FC<ITask> = (props): ReactElement => {
+  const {
+    title = 'Test Title',
+    date = new Date(),
+    description = 'Test Description',
+    priority = Priority.normal,
+    status = Status.completed,
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
+
   return (
     <Box
       display="flex"
@@ -18,12 +33,22 @@ export const Task: FC = (props): ReactElement => {
         backgroundColor: 'background.paper',
         borderRadius: '8px',
         border: '1px solid',
-        borderColor: 'error.light',
+        borderColor: renderPriorityBorderColor(priority),
       }}
     >
-      <TaskHeader />
-      <TaskDescription />
-      <TaskFooter />
+      <TaskHeader title={title} date={date} />
+      <TaskDescription description={description} />
+      <TaskFooter onClick={onClick} onStatusChange={onStatusChange} />
     </Box>
   );
+};
+
+Task.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.instanceOf(Date),
+  description: PropTypes.string,
+  onStatusChange: PropTypes.func,
+  onClick: PropTypes.func,
+  priority: PropTypes.string,
+  status: PropTypes.string,
 };
