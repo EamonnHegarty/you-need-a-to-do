4aeb38a6628bc719cb08acf17609243230854773
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-08-16 14:09:40 +0100

    reorder structure before redux

--- client/src/components/createTaskForm/CreateTaskForm.tsx
+++ client/src/components/createTaskForm/CreateTaskForm.tsx
@@ -1,13 +1,54 @@
 import React, { FC, ReactElement } from 'react';
-import { Box, Typography, Stack } from '@mui/material';
-
-import { TaskTitleField } from './TaskTitleField';
-import { TaskDescriptionField } from './TaskDescriptionField';
-import { TaskDateField } from './TaskDateField';
-import { TaskSelectField } from './TaskSelectField';
+import {
+  Box,
+  Typography,
+  Stack,
+  TextField,
+  SelectChangeEvent,
+  InputLabel,
+  Select,
+  FormControl,
+  MenuItem,
+} from '@mui/material';
 
 import { Status } from '../../enums/Status';
 import { Priority } from '../../enums/Priority';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { ISelectField } from '../../interfaces/ISelectField';
+
+const TaskSelectField: FC<ISelectField> = (props): ReactElement => {
+  const {
+    value = '',
+    label = 'Select Box',
+    name = 'selectBox',
+    items = [{ value: '', label: 'Add Items' }],
+    disabled = false,
+    onChange = (e: SelectChangeEvent) => console.log(e),
+  } = props;
+
+  return (
+    <FormControl fullWidth size="small">
+      <InputLabel id={`${name}-id`}>{label}</InputLabel>
+      <Select
+        labelId={`${name}-id`}
+        id={`${name}-id-select`}
+        value={value}
+        label={label}
+        name={name}
+        onChange={onChange}
+        disabled={disabled}
+      >
+        {items.map((item, index) => (
+          <MenuItem key={index} value={item.value}>
+            {item.label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
 
 export const CreateTaskFrom: FC = (): ReactElement => {
   return (
@@ -24,45 +65,48 @@ export const CreateTaskFrom: FC = (): ReactElement => {
       </Typography>
 
       <Stack sx={{ width: '100%' }} spacing={2}>
-        <TaskTitleField />
-        <TaskDescriptionField />
-        <TaskDateField />
+        <TextField
+          id="title"
+          label="Task Title"
+          placeholder="Task Title"
+          variant="outlined"
+          size="small"
+          name="title"
+          fullWidth
+        />
+
+        <TextField
+          id="Description"
+          name="description"
+          label="Description"
+          placeholder="Description"
+          variant="outlined"
+          size="small"
+          multiline
+          rows={4}
+          fullWidth
+        />
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker label="Task Date" format="DD/MM/YY" />
+        </LocalizationProvider>
+
         <Stack sx={{ width: '100%' }} direction="row" spacing={2}>
           <TaskSelectField
             label="Status"
             name="status"
-            items={[
-              {
-                value: Status.todo,
-                label: Status.todo.toUpperCase(),
-              },
-              {
-                value: Status.inProgress,
-                label: Status.inProgress.toUpperCase(),
-              },
-              {
-                value: Status.completed,
-                label: Status.completed.toUpperCase(),
-              },
-            ]}
+            items={Object.values(Status).map((status) => ({
+              value: status,
+              label: status.toUpperCase(),
+            }))}
           />
           <TaskSelectField
             label="Priority"
             name="priority"
-            items={[
-              {
-                value: Priority.low,
-                label: Priority.low,
-              },
-              {
-                value: Priority.normal,
-                label: Priority.normal,
-              },
-              {
-                value: Priority.high,
-                label: Priority.high,
-              },
-            ]}
+            items={Object.values(Priority).map((priority) => ({
+              value: priority,
+              label: priority.toUpperCase(),
+            }))}
           />
         </Stack>
       </Stack>
--- client/src/components/task/Task.tsx
+++ client/src/components/task/Task.tsx
@@ -1,13 +1,76 @@
 import React, { FC, ReactElement } from 'react';
-import { Box } from '@mui/material';
-import { TaskHeader } from './TaskHeader';
-import { TaskDescription } from './TaskDescription';
-import { TaskFooter } from './TaskFooter';
+import {
+  Box,
+  Button,
+  Chip,
+  FormControlLabel,
+  Switch,
+  Typography,
+} from '@mui/material';
 import { ITask } from '../../interfaces/ITask';
 import { Status } from '../../enums/Status';
 import { Priority } from '../../enums/Priority';
 import PropTypes from 'prop-types';
 import { renderPriorityBorderColor } from '../../helpers/renderPriorityBorderColor';
+import { format } from 'date-fns';
+import { ITaskHeader } from '../../interfaces/ITaskHeader';
+import { ITaskDescription } from '../../interfaces/ITaskDescription';
+import { ITaskFooter } from '../../interfaces/ITaskFooter';
+
+const TaskHeader: FC<ITaskHeader> = (props): ReactElement => {
+  const { title = 'Default Title', date = new Date() } = props;
+  return (
+    <Box display="flex" width="100%" justifyContent="space-between" mb={2}>
+      <Box>
+        <Typography variant="h6">{title}</Typography>
+      </Box>
+      <Box>
+        <Chip variant="outlined" label={format(date, 'PPP')} />
+      </Box>
+    </Box>
+  );
+};
+
+const TaskDescription: FC<ITaskDescription> = (props): ReactElement => {
+  const {
+    description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptate a fugit hic maiores necessitatibus.',
+  } = props;
+  return (
+    <Box>
+      <Typography>{description}</Typography>
+    </Box>
+  );
+};
+
+const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
+  const {
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      mt={2}
+    >
+      <FormControlLabel
+        label="In Progress"
+        control={<Switch onChange={(e) => onStatusChange(e)} color="warning" />}
+      />
+      <Button
+        variant="contained"
+        color="success"
+        size="small"
+        sx={{ color: '#ffffff' }}
+        onClick={(e) => onClick(e)}
+      >
+        Mark Complete
+      </Button>
+    </Box>
+  );
+};
 
 export const Task: FC<ITask> = (props): ReactElement => {
   const {
